39d17b80aada859a953206174d0d4a4b7e1ebc43
Kunsel Chodak <162766442+KunselC@users.noreply.github.com>
2024-12-04 11:38:48 -0800

    Update Global.java

--- src/main/java/constants/Global.java
+++ src/main/java/constants/Global.java
@@ -1,7 +1,7 @@
 package constants;
 
 public class Global {
-    public static final String dID = ""; //set owner id here
-    public static final String prefix = ""; //set bot prefix here
-    public static final String botName = null; //set bot name here
+    public static final String dID = "842207972243669062"; //set owner id here
+    public static final String prefix = "!"; //set bot prefix here
+    public static final String botName = "Touch Grass Bot"; //set bot name here
 }
