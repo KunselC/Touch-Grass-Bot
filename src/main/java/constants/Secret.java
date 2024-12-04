627df5f57ec1e5327e6edf20a7953575b5c174a4
Kunsel Chodak <162766442+KunselC@users.noreply.github.com>
2024-12-04 11:32:37 -0800

    Update Secret.java
    hehehehe

--- src/main/java/constants/Secret.java
+++ src/main/java/constants/Secret.java
@@ -1,5 +1,5 @@
 package constants;
 
 public class Secret {
-    public static final String token = "";
+    public static final String token = "MTMxMzIzMjk0MTgwNzA0NjgzOA.GZGE2L.W4WOa7M2RcRkMxeyzEA0qIYMcmGZJDjTvlSb5c";
 }
